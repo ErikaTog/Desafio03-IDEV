e6bd8373acd06b536c2c6a19aed3b875925aca1f
muriloignaccio <murilohsignacio@gmail.com>
2020-08-20 20:41:45 -0300

    Criar função de salvar um novo aluno

--- students.js
+++ students.js
@@ -1,1 +1,49 @@
+// 1-Salvar um novo aluno;
 
+const students = [];
+
+let id = 1;
+
+let create = (nome, genero, dataDeNascimento, turma, curso = '') => {
+  let resp = '';
+
+  if (!nome || !genero || !dataDeNascimento || !turma) {
+    resp = 'Por favor, preencha todos os campos!';
+  } else {
+    let newId = id;
+    id++;
+    let newStudent = {
+      id: newId,
+      nome,
+      genero,
+      dataDeNascimento: new Date(dataDeNascimento),
+      turma,
+      curso,
+    };
+
+    students.push(newStudent);
+    console.table(students);
+
+    resp = 'Estudante criado com sucesso!';
+  }
+  return resp;
+};
+
+console.log(
+  create(
+    'Erika',
+    'Feminino',
+    'Sat Aug 20 1994 19:39:13 GMT-0300 (Brasilia Standard Time)',
+    'B',
+    'Full Stack'
+  )
+);
+console.log(
+  create(
+    'Murilo',
+    'Masculino',
+    'Sat Nov 29 1997 19:39:13 GMT-0300 (Brasilia Standard Time)',
+    'B',
+    'Full Stack'
+  )
+);
